d019a084dff6d1fa6f5d6ea0991c05034794ee16
Suryatheboss10 <73601057+Suryatheboss10@users.noreply.github.com>
2021-04-28 10:07:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-function addUser(){
+function adduser(){
     username = document.getElementById("account_name").value;
     localStorage.setItem("account_name", username);
-    window.location="kwitter_page.html"
+    window.location="kwitter_room.html"
     }
 
